070bf846a99c97be311931dcfe3ac0ec6cf2939d
Abusayedshuvo <abusayedshuvo24@gmail.com>
2024-09-26 21:15:32 +0600

    task 5

--- index.js
+++ index.js
@@ -56,4 +56,24 @@ const phones = [
   { model: "Phone5", brand: "iphone", price: 105000 },
   { model: "Phone6", brand: "HTC", price: 48000 },
 ];
-console.log(findAveragePrice(phones));
+// console.log(findAveragePrice(phones));
+
+// Task 5
+function monthlySalary(employees) {
+  let totalYearlySalary = 0;
+  for (employee of employees) {
+    const currentSalary =
+      employee.starting + employee.experience * employee.increment;
+    totalYearlySalary += currentSalary;
+  }
+  return totalYearlySalary / 12;
+}
+
+const employees = [
+  { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+  { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+  { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+  { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+console.log(monthlySalary(employees));
